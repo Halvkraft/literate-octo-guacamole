5e751945af8e0ded45b397ae02b7dbc5b845caa3
Halvkraft <55437238+Halvkraft@users.noreply.github.com>
2022-04-06 16:38:44 +0200

    0.0
    Testing git

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # literate-octo-guacamole
 A first project designed to organize files on my computer
+
+Hello! 
---
+++ foldercreator.py
@@ -0,0 +1,5 @@
+# This program checks where the program is being run from. It's actually just for testing git.  
+
+import os
+path = os.getcwd()
+print ("The current directory is " + path)
